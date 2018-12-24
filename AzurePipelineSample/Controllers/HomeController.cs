df923e0c4727fd9681959bc5b4e7c6f5331712ed
Masui Masanori <masanori840816@gmail.com>
2018-12-25 06:00:07 +0900

    Add controller and view (#3)

--- AzurePipelineSample/Controllers/HomeController.cs
+++ AzurePipelineSample/Controllers/HomeController.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AzurePipelineSample.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AzurePipelineSample.Controllers
 {
     public class HomeController: Controller
     {
-
+        [Route("")]
+        public IActionResult Index()
+        {
+            ViewData["SampleUsers"] = Enumerable.Range(0, 5)
+                .Select(num => new User
+                {
+                    ID = num,
+                    Name = "User " + num,
+                    Description = "Hello World!!!"
+                })
+                .ToList();
+            return View("/Views/Index.cshtml");
+        }
     }
 }
