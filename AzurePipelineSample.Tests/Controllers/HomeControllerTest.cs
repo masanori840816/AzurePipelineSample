764d3281b24977467f953ef40d4c9549502a09b5
Masui Masanori <masanori840816@gmail.com>
2018-12-26 18:30:04 +0900

    add GetAllUsers

--- AzurePipelineSample.Tests/Controllers/HomeControllerTest.cs
+++ AzurePipelineSample.Tests/Controllers/HomeControllerTest.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using AzurePipelineSample.Controllers;
+﻿using AzurePipelineSample.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Xunit;
 
@@ -18,6 +16,14 @@ namespace AzurePipelineSample.Tests.Controllers
         public void Index_OpenAndGetType()
         {
             Assert.IsType<ViewResult>(_controller.Index());
+        }
+
+        [Fact]
+        public void GetAllUsers_GetMoreThanOneUsers()
+        {
+            var users = _controller.GetAllUsers();
+            Assert.NotNull(users);
+            Assert.True(users.Count > 0);
         }
     }
 }
